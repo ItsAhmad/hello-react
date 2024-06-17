70376f66bf5f969eca491ae229cb3d47529afc79
Ahmad <36176055+ItsAhmad@users.noreply.github.com>
2024-06-17 12:59:36 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
+
+const Application = () => {
+  const [name, setName] = useState("");
+
+  const reset = () => {
+    console.log("reset");
+    setName(""); 
+  };
+
+  return (
+    <main>
+      <input 
+        type="text" 
+        placeholder="Type your name" 
+        value = {name}
+        onChange={(event) => setName(event.target.value)}
+      />
+      <Button onClick={reset}>Reset</Button>
+      {name && <h1>Hello {name}</h1>}
+    </main>
+  );
+};
+
+ReactDOM.render(<Application />, document.getElementById("root"));
